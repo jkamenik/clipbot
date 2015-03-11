1d1c68482fee19a546503e906e104874c5566d13
James Brennan <james@jamesbrennan.ca>
2015-03-10 18:14:24 -0700

    Clean up router parser

--- resources/plugins/clipbot/plugins/router.clj
+++ resources/plugins/clipbot/plugins/router.clj
@@ -5,70 +5,93 @@
         [zetta.parser.seq
          :only (satisfy? not-char char string number word any-token
                 get put want-input? letter end-of-input
-                spaces skip-whitespaces)]
+                space spaces skip-whitespaces)]
         [zetta.combinators
-         :only (around many many-till many1 sep-by skip-many)])
+         :only (around many many-till many1 sep-by1 sep-by skip-many)])
   (:require
    [clojure.string :as str]
    [rx.lang.clojure.core :as rx]
    [clipbot.plugin :as plugin]
    [clipbot.types :refer :all]))
 
-(defrecord Param [key value])
-
 (def ^:private key-parser
   (around spaces
           (<$> str/join (many-till (not-char \space)
                                    (char \:)))))
 
-(def ^:private letter-or-digit
-  (satisfy? #(Character/isLetterOrDigit ^java.lang.Character %)))
-
-(def ^:private slash-or-hyphen
-  (<|> (char \/) (char \:)))
-
-;; TODO accept any non-space character
 (def ^:private value-parser
-  (<$> str/join (many (around spaces (<|> slash-or-hyphen letter-or-digit)))))
+  (<$> str/join (many1 (not-char \,))))
 
 (def ^:private param-parser
-  (<$> #(Param. %1 %2)
+  (<$> #(vector (keyword %1) %2)
        key-parser
        value-parser))
 
 (def ^:private params-parser
   (sep-by param-parser (char \,)))
 
-(defn parse-input [input]
+(defn parse-params [input]
   (:result (parse-once params-parser input)))
 
+(def ^:private prefix-parser
+  (*> (char \/) word))
+
+(defn merge-params [acc params]
+  (if (empty? params)
+    acc
+    (let [param (first params)]
+      (recur (merge acc param)
+             (rest params)))))
+
+(def zetta-chat-message-parser
+  (do-parser
+   [prefix prefix-parser
+    :cond [
+           (= prefix "docker")
+           [
+            _ spaces
+            category (always prefix)
+            type word
+            :cond [
+                   (= type "run")
+                   [params params-parser]
+
+                   (= type "help")
+                   [params (always {})]
+                   ]
+            ]
+
+           (= prefix "ruby")
+           [
+            _ space
+            cmd (<$> str/join (many any-token))
+            category (always "docker")
+            type (always "run")
+            params (always [{:cmd ["ruby" "-e" (str \' cmd \')]}
+                            {:image "ruby"}])
+            ]
+           ]
+    ]
+   (merge-params {:category (keyword category)
+                  :type (keyword type)}
+                 params)
+   ))
+
 (defn parse-chat-message [{:keys [payload] :as ev}]
-  (let [[task-name & args] (str/split (->> payload (re-seq #".*") first second)
-                                   #"\s+") ]
-    (merge ev
-          (cond
-             ;; (= task-name (:name PACKAGE))
-             ;; {:category :router
-             ;;  :type :package
-             ;;  :job-name (first args)}
-
-             ;; (= task-name (:name STATUS))
-             ;; {:category :router
-             ;;  :type :status
-             ;;  :job-name (first args)}
-
-             :else
-             {}))))
-
+  (->>
+    payload
+    (parse-once zetta-chat-message-parser)
+    :result
+    (merge ev)))
 
 (defn- category-type [category type]
   (fn -filter-msg [msg]
     (and (= (:category msg)) category
          (= (:type msg) type))))
 
-(defn chat-message-parser [{:keys [send-raw-event] :as ev*}]
-  (let [ev (parse-chat-message ev*)]
-    (send-raw-event ev)))
+(defn chat-message-parser [{:keys [send-chat-message send-raw-message] :as ev}]
+  ;; (send-chat-message (parse-chat-message ev))
+  (send-raw-message (parse-chat-message ev)))
 
 (defn init-router-bot [subscribe observable]
   (let [chat-message-events (rx/filter (category-type :chat :receive-message) observable)]
--- test/clipbot/plugins/router_test.clj
+++ test/clipbot/plugins/router_test.clj
@@ -2,9 +2,39 @@
   (:require [clojure.test :refer :all]
             [clipbot.plugins.router :as router]))
 
-(deftest test-params-parser
-  (are [input expected] (= (router/parse-input input) expected)
-       "key: value:latest, key-one: value/1" '({:key "key"
-                                               :value "value:latest"}
-                                              {:key "key-one"
-                                               :value "value/1"})))
+(deftest parse-chat-message-test
+  (are [input expected] (= (router/parse-chat-message {:payload input})
+                           expected)
+       "/docker run"
+       {:category :docker
+        :type :run
+        :payload "/docker run"}
+
+       "/docker run image: unbounce/base"
+       {:category :docker
+        :type :run
+        :image "unbounce/base"
+        :payload "/docker run image: unbounce/base"}
+
+       "/docker run image: unbounce/base, cmd: echo hello"
+       {:category :docker
+        :type :run
+        :image "unbounce/base"
+        :cmd "echo hello"
+        :payload "/docker run image: unbounce/base, cmd: echo hello"}
+
+       "/docker  run  image:  unbounce/base ,  cmd:  echo hello "
+       {:category :docker
+        :type :run
+        :image "unbounce/base "
+        :cmd "echo hello "
+        :payload "/docker  run  image:  unbounce/base ,  cmd:  echo hello "}
+
+
+       "/ruby puts hello"
+       {:category :docker
+        :type :run
+        :image "ruby"
+        :cmd ["ruby" "-e" (str "'puts hello'")]
+        :payload "/ruby puts hello"}
+       ))
